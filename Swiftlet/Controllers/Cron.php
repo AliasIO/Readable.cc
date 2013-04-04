1f0397780c71fb5c6c6ac82e5a29d691a4acc9df
Elbert Alias <elbert@alias.io>
2013-04-05 08:43:26 +1100

    Fixed learning

--- Swiftlet/Controllers/Cron.php
+++ Swiftlet/Controllers/Cron.php
@@ -19,10 +19,10 @@ class Cron extends \Swiftlet\Controller
 			INNER JOIN users_feeds ON       users.id      = users_feeds.user_id
 			INNER JOIN feeds       ON users_feeds.feed_id =       feeds.id
 			WHERE
-			  	users.enabled                 = 1                                                                                       AND -- Fetch feed for enabled users
-			  	users.last_active_at          > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 30 DAY)                          --                    AND -- Fetch feed for active users
-			--  ( feeds.last_fetch_attempted_at < DATE_SUB(UTC_TIMESTAMP(), INTERVAL  6 HOUR) OR  feeds.last_fetch_attempted_at IS NULL ) AND -- Fetch feeds four times a day
-			--	( feeds.last_fetched_at         > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 90 DAY)  OR  feeds.last_fetched_at         IS NULL )     -- Give up on feeds after three months of failed attempts
+			    users.enabled                 = 1                                                                                       AND -- Fetch feed for enabled users
+			    users.last_active_at          > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 30 DAY)                                              AND -- Fetch feed for active users
+			  ( feeds.last_fetch_attempted_at < DATE_SUB(UTC_TIMESTAMP(), INTERVAL  6 HOUR) OR  feeds.last_fetch_attempted_at IS NULL ) AND -- Fetch feeds four times a day
+			  ( feeds.last_fetched_at         > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 90 DAY)  OR  feeds.last_fetched_at         IS NULL )     -- Give up on feeds after three months of failed attempts
 			GROUP BY feeds.id
 			LIMIT 50
 			;');
@@ -56,34 +56,9 @@ class Cron extends \Swiftlet\Controller
 		}
 
 		// Learning
-		$learn = $this->app->getSingleton('learn');
+		$result = $this->app->getSingleton('learn')->learn($itemIds);
 
-		$sth = $dbh->prepare('
-			SELECT
-				users.id
-			FROM users
-			WHERE
-			  	enabled         = 1                                                                      AND -- Learn only for enabled users
-				( last_learned_at < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY) OR last_learned_at IS NULL ) AND -- Learn once a day at most
-			    last_active_at  > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)                                  -- Learn only for recently active users
-			LIMIT 1000
-			;');
-
-		$sth->execute();
-
-		$users = $sth->fetchAll(\PDO::FETCH_OBJ);
-
-		echo 'Learning for ' . count($users) . ' users&hellip;<br>';
-
-		foreach ( $users as $user ) {
-			$learn->user($user->id);
-		}
-
-		echo 'Learning for ' . count($itemIds) . ' items&hellip;<br>';
-
-		if ( $itemIds ) {
-			$learn->items($itemIds);
-		}
+		echo 'Learned for ' . $result[0] . ' items and ' . $result[1] . ' users<br>';
 
 		// Prune sessions
 		if ( $handle = opendir('sessions') ) {
--- Swiftlet/Models/Learn.php
+++ Swiftlet/Models/Learn.php
@@ -7,94 +7,114 @@ class Learn extends \Swiftlet\Model
 	/**
 	 * TODO
 	 *
-	 * @param int $userId
+	 * @param int $itemIds
 	 */
-	public function user($userId)
+	public function learn($itemIds)
 	{
 		$dbh = $this->app->getSingleton('pdo')->getHandle();
 
-		// Update user
+		// Update users
+		$userIds = array();
+
 		$sth = $dbh->prepare('
-			UPDATE users SET
-				last_learned_at = UTC_TIMESTAMP()
-			WHERE
-				id = :id
-			;');
-
-		$sth->bindParam('id', $userId);
-
-		$sth->execute();
-
-		// Rank words
-		$sth = $dbh->prepare('
-      REPLACE INTO users_words (
-        user_id,
-        word_id,
-        score
-      )
-      SELECT
-        main.user_id                     AS user_id,
-        main.word_id                     AS word_id,
-        main.vote * ( @row := @row + 1 ) AS score
-      FROM (
-        SELECT
-					words.id              AS word_id,
-					users_items.user_id   AS user_id,
-					SUM(users_items.vote) AS vote
-        FROM      words
-        LEFT JOIN items_words ON       words.id      = items_words.word_id
-        LEFT JOIN items       ON items_words.item_id =       items.id
-        LEFT JOIN users_items ON       items.id      = users_items.item_id
-        WHERE
-          users_items.user_id = :user_id
-        GROUP BY words.id
-        ORDER BY count DESC
-      ) AS main, (
-        SELECT @row := 0
-      ) AS rownum
-			;');
-
-		$sth->bindParam('user_id', $userId);
-
-		$sth->execute();
-	}
-
-	/**
-	 * TODO
-	 *
-	 * @param array $itemIds
-	 */
-	public function items($itemIds)
-	{
-		$dbh = $this->app->getSingleton('pdo')->getHandle();
-
-		// Rank items
-		$sth = $dbh->prepare('
-			INSERT INTO users_items (
-				user_id,
-				item_id,
-				score
-				)
 			SELECT
-				users_feeds.user_id,
-				items.id,
-				COALESCE(SUM(users_words.score), 0)
-			FROM       users_feeds
-			INNER JOIN       items ON items.feed_id       = users_feeds.feed_id
-			LEFT  JOIN users_items ON users_items.item_id =       items.id
-			LEFT  JOIN items_words ON items_words.item_id =       items.id
-			LEFT  JOIN users_words ON users_words.word_id = items_words.word_id
+				users.id
+			FROM users
 			WHERE
-				items.id IN ( ' . implode(', ', $itemIds) . ' )
-			GROUP BY users_feeds.user_id, items.id
-			ORDER BY created_at DESC
-			LIMIT 1000000
-			ON DUPLICATE KEY UPDATE
-				score = VALUES(score)
+			  	enabled         = 1                                                                      AND -- Learn only for enabled users
+				( last_learned_at < DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY) OR last_learned_at IS NULL ) AND -- Learn once a day at most
+			    last_active_at  > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)                                  -- Learn only for recently active users
+			LIMIT 1000
+			;');
+
+		$sth->execute();
+
+		$users = $sth->fetchAll(\PDO::FETCH_OBJ);
+
+		foreach ( $users as $user ) {
+			$userIds[] = $user->id;
+		}
+
+		if ( $userIds ) {
+			$sth = $dbh->prepare('
+				UPDATE users SET
+					last_learned_at = UTC_TIMESTAMP()
+				WHERE
+					users.id IN ( ' . implode(', ', $userIds) . ' )
+				LIMIT 1000
 				;');
 
-		$sth->bindParam('user_id', $userId);
+			$sth->execute();
 
-		$sth->execute();
+			// Rank words
+			foreach ( $userIds as $userId ) {
+				$sth = $dbh->prepare('
+					REPLACE INTO users_words (
+						user_id,
+						word_id,
+						score
+					)
+					SELECT
+						main.user_id                     AS user_id,
+						main.word_id                     AS word_id,
+						main.vote * ( @row := @row + 1 ) AS score
+					FROM (
+						SELECT
+							words.id              AS word_id,
+							users_items.user_id   AS user_id,
+							SUM(users_items.vote) AS vote
+						FROM      words
+						LEFT JOIN items_words ON       words.id      = items_words.word_id
+						LEFT JOIN items       ON items_words.item_id =       items.id
+						LEFT JOIN users_items ON       items.id      = users_items.item_id
+						WHERE
+							users_items.user_id = :user_id
+						GROUP BY words.id
+						ORDER BY count DESC
+					) AS main, (
+						SELECT @row := 0
+					) AS rownum
+					;');
+
+				$sth->bindParam('user_id', $userId);
+
+				$sth->execute();
+			}
+		}
+
+		// Rank items
+		if ( $itemIds ) {
+			$sth = $dbh->prepare('
+				INSERT INTO users_items (
+					user_id,
+					item_id,
+					score
+					)
+				SELECT
+					users_feeds.user_id,
+					items.id,
+					COALESCE(SUM(users_words.score), 0)
+				FROM       users_feeds
+				INNER JOIN       users ON users.id            = users_feeds.user_id
+				INNER JOIN       items ON items.feed_id       = users_feeds.feed_id
+				LEFT  JOIN users_items ON users_items.item_id =       items.id
+				LEFT  JOIN items_words ON items_words.item_id =       items.id
+				LEFT  JOIN users_words ON users_words.word_id = items_words.word_id
+				WHERE
+						items.id              IN ( ' . implode(', ', $itemIds) . ' )      AND -- Learn only for new items
+						users.enabled         = 1                                         AND -- Learn only for enabled users
+						users.last_active_at  > DATE_SUB(UTC_TIMESTAMP(), INTERVAL 1 DAY)     -- Learn only for recently active users
+				GROUP BY users_feeds.user_id, items.id
+				LIMIT 1000000
+				ON DUPLICATE KEY UPDATE
+					score = VALUES(score)
+				;');
+
+			$sth->bindParam('user_id', $userId);
+
+			$sth->execute();
+		}
+
+		return array(count($itemIds), count($userIds));
 	}
 }
