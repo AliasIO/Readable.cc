7f48bf31b7ddb41f7647db42626438f140b30cd4
Elbert Alias <elbert@alias.io>
2013-04-13 15:49:55 +1000

    Configurable meta description

--- views/header.html.php
+++ views/header.html.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 
-		<title><?php echo $this->htmlEncode($this->app->getConfig('siteName')) . ' - ' . $this->get('pageTitle') ?></title>
+		<title><?php echo $this->get('pageTitle') . ' - ' . $this->htmlEncode($this->app->getConfig('siteName')) ?></title>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
