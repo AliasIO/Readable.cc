69a972514e759b5770a6216ab419e3b1912100ce
Elbert Alias <elbert@alias.io>
2013-04-13 19:28:51 +1000

    Added scrolling text to page head

--- views/index.html.php
+++ views/index.html.php
@@ -6,7 +6,9 @@
 
 		<p>
 			<span>Navigate to next and previous articles with <code>j</code> and <code>k</code> (<a href="/shortcuts">more</a>).</span>
+			<?php if ( !$this->app->getSingleton('session')->get('id') ): ?>
 			<span><a href="/signup">Sign up</a> to subscribe to RSS feeds and vote on articles for personalised reading.</span>
+			<?php endif ?>
 		</p>
 	</div>
 </div>
