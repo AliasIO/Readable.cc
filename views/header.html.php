2fcf9251dbea59cc5fe9209ccebc4cd0b1c78651
Elbert Alias <elbert@alias.io>
2013-05-26 10:22:18 +1000

    Folders fully functional

--- views/header.html.php
+++ views/header.html.php
@@ -13,7 +13,7 @@
 
 		<link href="<?php echo $this->app->getRootPath() ?>views/lib/bootstrap/css/readable.css" rel="stylesheet">
 		<link href="<?php echo $this->app->getRootPath() ?>views/lib/entypo/entypo.css" rel="stylesheet">
-		<link href="<?php echo $this->app->getRootPath() ?>views/css/layout.css?e" rel="stylesheet">
+		<link href="<?php echo $this->app->getRootPath() ?>views/css/layout.css?f" rel="stylesheet">
 
 		<script>
 			var readable = {};
