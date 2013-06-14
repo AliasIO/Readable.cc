c39b0728f8760443d78c21568d014189dfbcf3ed
Elbert Alias <elbert@alias.io>
2013-06-14 19:57:55 +1000

    Added Piwik tracking code

--- views/footer.html.php
+++ views/footer.html.php
@@ -52,5 +52,22 @@
 				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 			})();
 		</script>
+
+		<!-- Piwik -->
+		<script type="text/javascript">
+			var _paq = _paq || [];
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.alias.io//";
+				_paq.push(['setTrackerUrl', u+'piwik.php']);
+				_paq.push(['setSiteId', 1]);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+				g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+			})();
+
+		</script>
+		<noscript><p><img src="http://piwik.alias.io/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+		<!-- End Piwik Code -->
 	</body>
 </html>
