7b2ec4b11afed177bc0d033c9bcb972a2f6a3d49
Elbert Alias <elbert@alias.io>
2014-11-24 15:39:15 +1100

    CF cookie

--- views/footer.php
+++ views/footer.php
@@ -41,7 +41,7 @@
 			_paq.push(['trackPageView']);
 			_paq.push(['enableLinkTracking']);
 			(function() {
-				var u=("https://alias.io/piwik/";
+				var u=("https://alias.io/piwik/");
 				_paq.push(['setTrackerUrl', u+'piwik.php']);
 				_paq.push(['setSiteId', 1]);
 				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
